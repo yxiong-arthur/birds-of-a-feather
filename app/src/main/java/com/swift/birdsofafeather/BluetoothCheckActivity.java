99e4becae2e36ec8a7a9e1a10bb6673938b6a077
Darren Wu <wudarre0908@gmail.com>
2022-01-30 00:58:48 -0800

    Created Activity for Checking Bluetooth

---
+++ app/src/main/java/com/swift/birdsofafeather/BluetoothCheckActivity.java
@@ -0,0 +1,14 @@
+package com.swift.birdsofafeather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BluetoothCheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_check);
+    }
+}
