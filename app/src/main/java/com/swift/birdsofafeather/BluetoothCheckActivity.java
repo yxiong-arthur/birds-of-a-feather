cbe1ce26e2c063343f2ba7c049c0d4817cf853f2
Darren Wu <wudarre0908@gmail.com>
2022-02-02 21:16:37 -0800

    Initial Work on Bluetooth Permission Request

--- app/src/main/java/com/swift/birdsofafeather/BluetoothCheckActivity.java
+++ app/src/main/java/com/swift/birdsofafeather/BluetoothCheckActivity.java
@@ -1,14 +1,35 @@
 package com.swift.birdsofafeather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class BluetoothCheckActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_check);
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
 }
