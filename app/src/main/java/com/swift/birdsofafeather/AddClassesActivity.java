2583f54c664093dbde7782e589489b357730f331
Travis Salud <tsalud@ucsd.edu>
2022-02-05 22:25:46 -0800

    Added spinners content #4 #29

--- app/src/main/java/com/swift/birdsofafeather/AddClassesActivity.java
+++ app/src/main/java/com/swift/birdsofafeather/AddClassesActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.swift.birdsofafeather.model.DummyClass;
 
 public class AddClassesActivity extends AppCompatActivity {
 
@@ -12,9 +17,34 @@ public class AddClassesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_classes);
+
+        Spinner yearSpinner = findViewById(R.id.year_select);
+        ArrayAdapter<CharSequence> yearAdapter = ArrayAdapter.createFromResource(this,
+                R.array.years_array, android.R.layout.simple_spinner_item);
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        yearSpinner.setAdapter(yearAdapter);
+
+        Spinner quarterSpinner = findViewById(R.id.quarter_select);
+        ArrayAdapter<CharSequence> quarterAdapter = ArrayAdapter.createFromResource(this,
+                R.array.quarters_array, android.R.layout.simple_spinner_item);
+        quarterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        quarterSpinner.setAdapter(quarterAdapter);
+
     }
 
     protected void onEnterClicked(View view){
+        Spinner yearSpinner = findViewById(R.id.year_select);
+        Spinner quarterSpinner = findViewById(R.id.quarter_select);
         TextView subjectTextView = (TextView) findViewById(R.id.subject_textview);
+        TextView courseNumberTextView = (TextView) findViewById(R.id.courseNumber_textview);
+
+        int id = 15;
+        String yearString = yearSpinner.getSelectedItem().toString();
+        int year = Integer.parseInt(yearString);
+        String quarter = quarterSpinner.getSelectedItem().toString();
+        String subject = subjectTextView.getText().toString();
+        String courseNumber = courseNumberTextView.getText().toString();
+
+        DummyClass dummyClass = new DummyClass(id, year, quarter, subject, courseNumber);
     }
 }
