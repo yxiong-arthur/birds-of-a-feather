7d253126415174912d98df53b5dcf1a8a2b858e1
Darren Wu <wudarre0908@gmail.com>
2022-02-06 00:09:00 -0800

    Removed Mockito from testing class

--- app/src/test/java/com/swift/birdsofafeather/NameActivityTest.java
+++ app/src/test/java/com/swift/birdsofafeather/NameActivityTest.java
@@ -6,8 +6,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-
 
 import static org.junit.Assert.*;
 
@@ -23,8 +21,6 @@ import androidx.lifecycle.Lifecycle;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import static org.mockito.Mockito.*;
-import org.mockito.MockedStatic;
 
 @RunWith(AndroidJUnit4.class)
 public class NameActivityTest {
