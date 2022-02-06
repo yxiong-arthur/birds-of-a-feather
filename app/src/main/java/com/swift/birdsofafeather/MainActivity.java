f2537d6c6cdab544b57d766cfbed7944fe261859
Arthur-XYL <96358408+Arthur-XYL@users.noreply.github.com>
2022-02-06 12:30:44 -0800

    Finish task 3.1 and 3.3.

--- app/src/main/java/com/swift/birdsofafeather/MainActivity.java
+++ app/src/main/java/com/swift/birdsofafeather/MainActivity.java
@@ -2,13 +2,23 @@ package com.swift.birdsofafeather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onLaunchPhotoClicked(View view) {
+        Intent intent = new Intent(this, PhotoUpload.class);
+        startActivity(intent);
+    }
+
+
 }
