af2dc6353412a133de307a78cf00f6c845d127f8
dwu <wudarren0908@gmail.com>
2022-02-07 16:03:33 -0800

    Added Photo Upload Activity to Main dashboard

--- app/src/main/java/com/swift/birdsofafeather/MainActivity.java
+++ app/src/main/java/com/swift/birdsofafeather/MainActivity.java
@@ -29,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLaunchPhotoClicked(View view) {
-        //Intent intent = new Intent(this, NameActivity.class);
-        //startActivity(intent);
+        Intent intent = new Intent(this, PhotoUpload.class);
+        startActivity(intent);
     }
 
     public void onLaunchAddClassesClicked(View view) {
