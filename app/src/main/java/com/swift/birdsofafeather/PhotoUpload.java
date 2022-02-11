309e663aaffea105c74c7cefaa21d30b1163173b
Arthur-XYL <96358408+Arthur-XYL@users.noreply.github.com>
2022-02-10 17:33:35 -0800

    try to fix image

--- app/src/main/java/com/swift/birdsofafeather/PhotoUpload.java
+++ app/src/main/java/com/swift/birdsofafeather/PhotoUpload.java
@@ -2,31 +2,37 @@ package com.swift.birdsofafeather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.swift.birdsofafeather.databinding.ActivityMainBinding;
+import com.bumptech.glide.request.RequestOptions;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+
+
+
 
 public class PhotoUpload extends AppCompatActivity {
 
     EditText loadURL;
     Button saveButton, loadButton;
     ImageView imageResult;
+    Button downloadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +44,26 @@ public class PhotoUpload extends AppCompatActivity {
         loadButton = findViewById(R.id.loadPhotoButton);
         loadURL = (EditText) findViewById(R.id.photoUploadURL);
         imageResult = findViewById(R.id.uploadedPhoto);
+        downloadButton = findViewById(R.id.downloadPhotoButton);
+        ImageView downloadPhoto = findViewById(R.id.downloadPhoto);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String URLlink = loadURL.getText().toString();
-                if (URLlink.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please enter url", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    LoadImage image = new LoadImage(imageResult);
-                }
+                imageResult.buildDrawingCache();
+                Bitmap bmap = imageResult.getDrawingCache();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] b = baos.toByteArray();
+
+                String encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+
+                SharedPreferences shre = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor edit = shre.edit();
+                edit.putString("image_data",encodedImage);
+                edit.apply();
+                downloadPhoto.setImageBitmap(null);
+                downloadPhoto.setImageResource(0);
             }
         });
 
@@ -61,13 +76,28 @@ public class PhotoUpload extends AppCompatActivity {
 
                 if(URLLink.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Please enter url", Toast.LENGTH_SHORT).show();
-                }else {
-                    Glide.with(PhotoUpload.this).load(URLLink).into(imageResult );
+                }
+
+                else {
+                    Glide.with(PhotoUpload.this).load(URLLink).into(imageResult);
+                }
+            }
+        });
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences shre = getPreferences(MODE_PRIVATE);
+                String previouslyEncodedImage = shre.getString("image_data", "");
+
+                if( !previouslyEncodedImage.equalsIgnoreCase("") ){
+                    byte[] b = Base64.decode(previouslyEncodedImage, Base64.DEFAULT);
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
+                    downloadPhoto.setImageBitmap(bitmap);
                 }
             }
         });
     }
-
 
     private class LoadImage extends AsyncTask<String,Void, Bitmap> {
         ImageView imageView;
